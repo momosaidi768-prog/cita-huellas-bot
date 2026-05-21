653bfc5bd708cfa259005b44a508ea1e343f4144
momosaidi768-prog <momosaidi768@gmail.com>
2026-05-21 10:28:19 +0200

    Update main.py
    H

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from playwright.async_api import async_playwright
 TOKEN = os.getenv("8202293986:AAEnZuCcvl6Gf98Th9b6hnfj3ZLg6gmnC5k")
 ADMIN_ID = int(os.getenv("6675176280"))
 
-def is_admin(user_id):
+def is_admin(user_id: int):
     return user_id == ADMIN_ID
 
 # ================= PLAYWRIGHT =================
@@ -29,14 +29,16 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not is_admin(update.effective_user.id):
         return await update.message.reply_text("⛔ Not allowed")
 
-    await update.message.reply_text("Bot is running ✔\nUse /open https://example.com")
+    await update.message.reply_text(
+        "Bot is running ✔\nUse:\n/open https://example.com"
+    )
 
 async def open_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not is_admin(update.effective_user.id):
         return await update.message.reply_text("⛔ Not allowed")
 
     if not context.args:
-        return await update.message.reply_text("Use: /open https://example.com")
+        return await update.message.reply_text("Usage: /open https://example.com")
 
     url = context.args[0]
 
