b1c515b121565e47eeaf64e9842be8986fccc9f1
momosaidi768-prog <momosaidi768@gmail.com>
2026-05-21 09:53:02 +0200

    Update main.py
    add requirements file

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from playwright.async_api import async_playwright
@@ -7,38 +6,35 @@ from playwright.async_api import async_playwright
 TOKEN = os.getenv("8202293986:AAEnZuCcvl6Gf98Th9b6hnfj3ZLg6gmnC5k")
 ADMIN_ID = int(os.getenv("6675176280"))
 
-# ================= CHECK ADMIN =================
-
+# ================= ADMIN =================
 def is_admin(user_id):
     return user_id == ADMIN_ID
 
 # ================= PLAYWRIGHT =================
-
 async def open_site(url: str):
     async with async_playwright() as p:
         browser = await p.chromium.launch(
             headless=True,
-            args=["--no-sandbox"]
+            args=["--no-sandbox", "--disable-setuid-sandbox"]
         )
 
         page = await browser.new_page()
-        await page.goto(url)
+        await page.goto(url, timeout=60000)
 
         await page.screenshot(path="screen.png")
 
         await browser.close()
 
 # ================= COMMANDS =================
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not is_admin(update.effective_user.id):
-        return await update.message.reply_text("⛔ Not authorized")
+        return await update.message.reply_text("⛔ Not allowed")
 
-    await update.message.reply_text("Bot running ✔\nUse /open https://site.com")
+    await update.message.reply_text("✔ Bot running\nUse /open https://site.com")
 
 async def open_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not is_admin(update.effective_user.id):
-        return await update.message.reply_text("⛔ Not authorized")
+        return await update.message.reply_text("⛔ Not allowed")
 
     if not context.args:
         return await update.message.reply_text("Example: /open https://example.com")
@@ -47,15 +43,20 @@ async def open_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text("Opening site...")
 
-    await open_site(url)
-
-    await update.message.reply_photo(photo=open("screen.png", "rb"))
+    try:
+        await open_site(url)
+        await update.message.reply_photo(photo=open("screen.png", "rb"))
+    except Exception as e:
+        await update.message.reply_text(f"Error: {e}")
 
 # ================= RUN =================
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
 
-app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("open", open_cmd))
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("open", open_cmd))
+    app.run_polling()
 
-app.run_polling()
+if __name__ == "__main__":
+    main()
